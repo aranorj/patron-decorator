2f1fd08486ee0d9a9732e74269a3244c62b32de3
aranorj <arano.rocio.b@gmail.com>
2024-09-13 23:50:41 -0300

    feature: habilidades del nuevo mago

--- src/Mago.java
+++ src/Mago.java
@@ -2,4 +2,7 @@ public class Mago extends Personaje{
     public Mago(String descripcion, int puntosDeVida) {
         super(descripcion, puntosDeVida);
     }
+
+
+    // le agrego funcionalidad al mago
 }
