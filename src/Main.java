3af324b14829e901e0881630b837e866696e5a62
aranorj <arano.rocio.b@gmail.com>
2024-09-13 22:50:24 -0300

    feat: crear un enemigo Goblin

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         //Empieza el juego
         Personaje miAvatar = new Guerrero();
+        //Creo un anemigo
+        Personaje enemigo = new Goblin();
         System.out.println(miAvatar.getDescripcion() + " tiene " + miAvatar.getPuntosDeVida() + " punto de vida");
+        System.out.println("El enemigo tiene " + enemigo.getPuntosDeVida() + " punto de vida");
 
         //Consigo una armadura
         Personaje avatarConArmadura = new ArmaduraDeTitanio(miAvatar);
